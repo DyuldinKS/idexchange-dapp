b1aebd9c1ab87bb1503d914484bf8e39c50e6113
Kirill Dyuldin <dyuldinks@gmail.com>
2023-03-20 00:59:22 +0530

    add custom mui theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,3 +1,38 @@
 import { createTheme } from '@mui/material/styles';
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#607d8b',
+    },
+    secondary: {
+      main: '#4db6ac',
+    },
+    background: {
+      default: '#f7f7f7',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '3.2rem',
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: '2.4rem',
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: '2rem',
+      fontWeight: 700,
+    },
+    h4: {
+      fontSize: '1.6rem',
+    },
+    h5: {
+      fontSize: '1.4rem',
+    },
+    h6: {
+      fontSize: '1.2rem',
+    },
+  },
+});
